6b9df319bafdbbdc4c7126609a58f8dea5e6ccba
CANDYFLOSSKKI <77969014+CANDYFLOSSKKI@users.noreply.github.com>
2025-01-19 20:08:18 +0800

    25/01/19 fix controller config

--- cp-web/src/main/java/com/ctey/cpweb/Controller/CPTaskController.java
+++ cp-web/src/main/java/com/ctey/cpweb/Controller/CPTaskController.java
@@ -4,14 +4,12 @@ import com.ctey.cpmodule.Service.ReqWorkService;
 import com.ctey.cpstatic.Entity.TaskStartReq;
 import com.feiniaojin.gracefulresponse.GracefulResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class CPTaskController {
     private final ReqWorkService reqWorkService;
 
