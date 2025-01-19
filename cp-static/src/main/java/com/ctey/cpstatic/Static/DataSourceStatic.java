765475ec59b116256f0b9112cfb12df770f2ac37
CANDYFLOSSKKI <77969014+CANDYFLOSSKKI@users.noreply.github.com>
2025-01-19 21:09:22 +0800

    25/01/19 add params secret

--- cp-static/src/main/java/com/ctey/cpstatic/Static/DataSourceStatic.java
+++ cp-static/src/main/java/com/ctey/cpstatic/Static/DataSourceStatic.java
@@ -2,12 +2,12 @@ package com.ctey.cpstatic.Static;
 
 public class DataSourceStatic {
     // 数据源JDBC URL
-    // Example: jdbc:mysql://172.28.64.1:3308/?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&allowMultiQueries=true&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true
-    public static final String DATASOURCE_URL = "jdbc:mysql://172.28.64.1:3308/?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&allowMultiQueries=true&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    // Example: jdbc:mysql://127.0.0.1:3306/?useUnicode=true&characterEncoding=UTF-8&allowPublicKeyRetrieval=true
+    public static final String DATASOURCE_URL = "XXX";
     // 数据源用户名
     // Example: root
-    public static final String DATASOURCE_USER = "root";
+    public static final String DATASOURCE_USER = "XXX";
     // 数据源密码
     // Example: 123456
-    public static final String DATASOURCE_PASSWD = "123456";
+    public static final String DATASOURCE_PASSWD = "XXX";
 }
